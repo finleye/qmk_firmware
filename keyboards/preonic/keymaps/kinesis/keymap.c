ac3926b992e54081f40130b45808b0845425a12f
Corey Finley <finley.corey@gmail.com>
2016-09-07 09:34:20 -0400

    updates

--- keyboards/preonic/keymaps/kinesis/keymap.c
+++ keyboards/preonic/keymaps/kinesis/keymap.c
@@ -17,14 +17,8 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define HYP_TIC ALL_T(KC_GRV)
-#define EQ_LOCK TD(TD_EQ_LOCK)
-#define MIN_ULK TD(TD_MIN_ULOCK)
-
-//Tap Dance Declarations
-enum {
-  TD_EQ_LOCK = 0,
-  TD_MIN_ULOCK
-};
+#define HYP_TAB ALL_T(KC_TAB)
+#define CLT_ESC CTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -42,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = {
-    {EQ_LOCK, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    MIN_ULK },
-    {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS },
-    {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
+    {KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS },
+    {HYP_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS },
+    {CLT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
     {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC },
     {MO(_FN), HYP_TIC, KC_LALT, KC_LGUI, KC_BSPC, KC_ESC,  KC_ENT,  KC_SPC,  KC_RGUI, KC_LBRC, KC_RBRC, MO(_FN) }
   },
@@ -57,17 +51,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |      | VOL- | VOL+ | MUTE |BLtogg|      | LEFT | DOWN |  UP  | RGHT |      |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      | BL-  | BL+  |Aud on|AudOff|AGnorm|AGswap|      |      |      |      |      |
+   * |      | BL-  | BL+  |Aud on|AudOff|      |      |      |      |      |      |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |Voice-|Voice+|Mus on|MusOff|      |      |PLY/PS|      | PREV | NEXT |      |
+   * |      | tild |      |Mus on|MusOff|      |      |PLY/PS|      | PREV | NEXT |      |
    * `-----------------------------------------------------------------------------------'
    */
   [_FN] = {
-    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______ },
-    {_______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______ },
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PGUP },
+    {_______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_PGDN },
     {_______, KC_VOLD, KC_VOLU, KC_MUTE, BL_TOGG, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______ },
-    {_______, BL_DEC,  BL_INC,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______ },
-    {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  _______, _______, KC_MPLY, _______, KC_MPRV, KC_MNXT, _______ }
+    {_______, BL_DEC,  BL_INC,  AU_ON,   AU_OFF,  _______, _______, _______, _______, _______, M(_ULCK), _______ },
+    {_______, KC_TILD, _______, MU_ON,   MU_OFF,  _______, _______, KC_MPLY, _______, KC_MPRV, KC_MNXT, _______ }
   }
 
 };
@@ -86,6 +80,9 @@ void persistant_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
+
+const qk_tap_dance_action_t tap_dance_actions[] = {
+};
 
 /*bool process_record_user(uint16_t keycode, keyrecord_t *record) {*/
   /*return true;*/
@@ -122,21 +119,13 @@ void music_scale_user(void)
   PLAY_NOTE_ARRAY(music_scale, false, 0);
 }
 
-
-const qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for equal, twice for hyper + X (alfred lock)
-  [TD_EQ_LOCK] = ACTION_TAP_DANCE_DOUBLE(KC_EQL,  HYPR(KC_X)),
-  //Tap once for minus, twice for time.heals.nothing
-  [TD_MIN_ULOCK] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, M(_ULCK))
-};
-
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   switch(id) {
     case _ULCK:
       if (record->event.pressed) {
         /* time.heals.nothing */
-        return MACRO( I(220), T(T), T(I), T(M), T(E), T(DOT), T(H), T(E), T(A), T(L), T(S), T(DOT), T(N), T(O), T(T), T(H), T(I), T(N), T(G), END);
+        return MACRO( I(10), T(T), T(I), T(M), T(E), T(DOT), T(H), T(E), T(A), T(L), T(S), T(DOT), T(N), T(O), T(T), T(H), T(I), T(N), T(G), T(ENTER), END);
       }
       break;
   }
